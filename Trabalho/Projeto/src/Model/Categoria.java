e8e9185eec3c5c89604859876915ecdd6eacd740
Samuel José Candido <101359102+SamuelCandido@users.noreply.github.com>
2024-11-09 01:37:05 +0000

    Adicionado arquivos de enumeração: Categoria.enum e TipoLancamento.enum

---
+++ Trabalho/Projeto/src/Model/Categoria.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Win11
+ */
+public enum Categoria {
+    SALARIO,
+    DECIMO_TERCEIRO,
+    FERIAS,
+    ALIMENTACAO,
+    TRANSPORTE,
+    RESIDENCIA,
+    SAUDE,
+    EDUCACAO,
+    ENTRETERIMENTO
+}
